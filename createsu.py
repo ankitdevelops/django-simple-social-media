6dfe2891e09492750923cdc61c47b214ef43cc48
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-04-12 14:06:01 +0530

    fixing deployment issues

--- createsu.py
+++ createsu.py
@@ -8,7 +8,7 @@ class Command(BaseCommand):
     help = "Creates a superuser."
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username="admin").exists():
+        if not User.objects.filter(username="ankit").exists():
             User.objects.create_superuser(
                 username="ankit",
                 name="Ankit Kumar",
