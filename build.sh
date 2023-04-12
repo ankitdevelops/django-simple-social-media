8434df272750f95b6464880b2f4db8492816745a
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-04-12 14:21:41 +0530

    fixing deployment issues

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 python manage.py collectstatic --no-input
 python manage.py makemigrations post user
 python manage.py migrate
-python manage.py createsu 
+python  createsu 
