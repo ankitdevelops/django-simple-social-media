501290efad6a361572541032c17f20f6c9dc806b
Ankit Kumar <46848486+ankitdevelops@users.noreply.github.com>
2023-04-12 13:43:31 +0530

    .

---
+++ createsu.py
@@ -0,0 +1,18 @@
+from django.contrib.auth import get_user_model
+from django.core.management.base import BaseCommand
+
+User = get_user_model()
+
+
+class Command(BaseCommand):
+    help = "Creates a superuser."
+
+    def handle(self, *args, **options):
+        if not User.objects.filter(username="admin").exists():
+            User.objects.create_superuser(
+                username="ankit",
+                name="Ankit Kumar",
+                email="ankit@gmail.com",
+                password="testing321",
+            )
+        print("Superuser has been created.")
